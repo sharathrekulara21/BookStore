ec71a1a363311f7ce258bbcc0d623b95c214ffac
Sharath <sharath3634@gmail.com>
2023-12-11 12:24:28 +0530

    Update config.js

--- server/config.js
+++ server/config.js
@@ -1,4 +1,4 @@
 export const PORT = 5000;
 
 export const mongodbURL =
-	"mongodb+srv://Sharath:sharath2004@cluster0.bg37htj.mongodb.net/?retryWrites=true&w=majority";
+	"mongodb+srv://<Username>:<Password>@cluster0.bg37htj.mongodb.net/?retryWrites=true&w=majority";
